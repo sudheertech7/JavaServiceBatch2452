b92f9a7f3ace5b11e76416bfc46ba5b76295a16a
pavulurisudheer <121080655+pavulurisudheer@users.noreply.github.com>
2025-01-22 10:51:11 +0530

    mobile code

--- src/com/savitha/oops/MainMobile.java
+++ src/com/savitha/oops/MainMobile.java
@@ -4,11 +4,10 @@ class Mobile{
 	String mobileCompany;
 	int mobileRam;
 	int mobileRom;
-	
-	public Mobile(String mComapny, int mRam, int mRom) {
-		mobileCompany = mComapny;
-		mobileRam = mRam;
-		mobileRom = mRom;
+	public Mobile(String mobileCompany, int mobileRam, int mobileRom) {
+		this.mobileCompany = mobileCompany;
+		this.mobileRam = mobileRam;
+		this.mobileRom = mobileRom;
 	}
 	
 	public void printMobileInfo(){
@@ -19,12 +18,8 @@ public class MainMobile {
 	
 	public static void main(String[] args) {
 		Mobile pixel = new Mobile("pixel",8,128);
-		Mobile iphone = new Mobile("iphone", 6, 256);
 		System.out.println(pixel.mobileCompany);
 		pixel.printMobileInfo();
-		System.out.println("----------------------");
-		System.out.println(iphone.mobileCompany);
-		iphone.printMobileInfo();
 		
 	}
 }
