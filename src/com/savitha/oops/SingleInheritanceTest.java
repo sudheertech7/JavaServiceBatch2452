5c7dc4aeff2ae94077115cec2a8e4dd411073f5b
pavulurisudheer <121080655+pavulurisudheer@users.noreply.github.com>
2025-01-23 09:47:41 +0530

    Single Inheritance

---
+++ src/com/savitha/oops/SingleInheritanceTest.java
@@ -0,0 +1,38 @@
+package com.savitha.oops;
+class A{
+	//properties
+	int a = 10;
+	int b = 20;
+	//methods
+	void add() {
+		System.out.println("ADD Method From CLASS A");
+		System.out.println(a+b);
+	}
+}
+class B extends A{
+	//properties
+	int c = 200;
+	int d = 100;
+	//methods
+	void sub() {
+		System.out.println("SUB Method From CLASS B");
+		System.out.println(c-d);
+	}	
+}
+public class SingleInheritanceTest {
+	
+	public static void main(String[] args) {
+		B obj = new B();
+		
+		System.out.println(obj.c);
+		System.out.println(obj.d);
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		System.out.println("----------------------");
+		obj.sub();
+		obj.add();
+		
+	}
+	
+
+}
